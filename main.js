46894be739cce654d1c0ac561405c3d9e228c8df
Patnzee <nzimandepatriciasn@gmail.com>
2025-06-09 16:23:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// This variable holds the beginning of the sentence
+let Sent1 = "This is the beginning of the string";
+
+// This variable holds the end of the sentence
+let Sent2 = " and this is the end of the string";
+
+// Concatenate the two variables and display the result
+document.write(Sent1 + Sent2);
+
+// An expression: basic math
+let result = 8 + 2; // This adds 8 and 2
+
+// Displaying the result of the expression
+document.write("<br>The result of 8 + 2 is: " + result);
+
+// Another example using alert
+window.alert("All code executed successfully!");
+
