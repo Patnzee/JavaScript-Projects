29319e822816b5d4ecbe70870e1ea274b8d43bba
Patnzee <nzimandepatriciasn@gmail.com>
2025-06-26 07:12:44 +0100

    Elements Challenge

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-// Function to append text using += operator
-function addGreeting() {
-    // Get the HTML element with the id "message"
-    let element = document.getElementById("message");
-
-    // Append additional text using += operator
-    element.innerText += " Welcome to JavaScript!";
-}
+// DOM Document Object Example
+document.addEventListener("DOMContentLoaded", function () {
+    // Using document.getElementById and innerHTML
+    document.getElementById("demo").innerHTML = "The content has been changed using DOM!";
+});
